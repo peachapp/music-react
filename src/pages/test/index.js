f58e844397f8a42469652adf643ba2f3ef3e3d67
taotao <571596435@qq.com>
2021-02-25 09:32:25 +0800

    feat: 增加lodash和axios依赖

--- src/pages/test/index.js
+++ src/pages/test/index.js
@@ -1,6 +1,5 @@
-import Loading from 'components/loading/index';
 import { Button } from 'antd-mobile';
-
+import Loading from 'components/loading/index';
 
 const Test = () => {
   return <div className="container test-container">test<Loading /><Button type="primary">按钮</Button></div>
--- src/routers.js
+++ src/routers.js
@@ -5,6 +5,7 @@ import { HashRouter, Switch, Route, Redirect } from 'react-router-dom';
 //Switch路由唯一匹配
 //Route配置路由规则
 //Redirect跳转错误显示的页面
+import Loading from 'components/loading/index';
 const Login = lazy(() => import('pages/login/index'));
 const Home = lazy(() => import('pages/home/index'));
 const Test = lazy(() => import('pages/test/index'));
@@ -12,7 +13,7 @@ const NotFound = lazy(() => import('pages/notFound/index'));
 
 const Routers = () => {
   return <HashRouter>
-    <Suspense fallback={<div>Loading...</div>}>
+    <Suspense fallback={<Loading />}>
       <Switch>
         <Route path='/' exact component={Home}></Route>
         <Route path='/login' component={Login}></Route>
