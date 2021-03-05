449a716e462dad27bd291745fe26ddbe260b7207
taotao <571596435@qq.com>
2021-03-05 17:14:44 +0800

    feat: 发现页(未完)

--- src/pages/home/components/faxian/index.js
+++ src/pages/home/components/faxian/index.js
@@ -1,13 +1,77 @@
 import React, { useState, useEffect, useCallback } from 'react';
+import { Carousel } from 'zarm';
 import './index.less';
 
 const Faxian = (props) => {
   // props
+  const { banners = ['https://static.zhongan.com/website/health/zarm/images/banners/1.png',
+    'https://static.zhongan.com/website/health/zarm/images/banners/2.png',
+    'https://static.zhongan.com/website/health/zarm/images/banners/3.png',] } = props;
 
   // data
 
-  return <div className="find-container">
-    发现页面
+  // renderFunction
+  const renderBanners = () => {
+    return <Carousel className="banner-container">
+      {
+        banners.map((bannerItem, bannerIndex) => {
+          return (
+            <div className="banner-item" key={bannerIndex}>
+              <img className="banner-img" src={bannerItem} alt="" draggable={false} />
+            </div>
+          );
+        })
+      }
+    </Carousel>
+  };
+
+  return <div className="faxian-container">
+    <div className="faxian-header">
+      <div className="faxian-header-left">
+        <i className="iconfont icongengduo1 faxian-header-left-icon" />
+      </div>
+      <div className="faxian-header-center">
+        <i className="iconfont iconsousuo faxian-header-center-icon" />大家都在搜 进击的巨人
+      </div>
+      <div className="faxian-header-right">
+        <i className="iconfont iconaudio-fill faxian-header-right-icon" />
+      </div>
+    </div>
+    {renderBanners()}
+    <div className="menu-container">
+      <div className="menu-item">
+        <img className="menu-img" src="https://static.zhongan.com/website/health/zarm/images/banners/1.png" alt="" />
+        <span className="menu-text">每日推荐</span>
+      </div>
+      <div className="menu-item">
+        <img className="menu-img" src="https://static.zhongan.com/website/health/zarm/images/banners/1.png" alt="" />
+        <span className="menu-text">私人FM</span>
+      </div>
+      <div className="menu-item">
+        <img className="menu-img" src="https://static.zhongan.com/website/health/zarm/images/banners/1.png" alt="" />
+        <span className="menu-text">歌单</span>
+      </div>
+      <div className="menu-item">
+        <img className="menu-img" src="https://static.zhongan.com/website/health/zarm/images/banners/1.png" alt="" />
+        <span className="menu-text">排行榜</span>
+      </div>
+      <div className="menu-item">
+        <img className="menu-img" src="https://static.zhongan.com/website/health/zarm/images/banners/1.png" alt="" />
+        <span className="menu-text">直播</span>
+      </div>
+      <div className="menu-item">
+        <img className="menu-img" src="https://static.zhongan.com/website/health/zarm/images/banners/1.png" alt="" />
+        <span className="menu-text">数字专辑</span>
+      </div>
+      <div className="menu-item">
+        <img className="menu-img" src="https://static.zhongan.com/website/health/zarm/images/banners/1.png" alt="" />
+        <span className="menu-text">歌房</span>
+      </div>
+      <div className="menu-item">
+        <img className="menu-img" src="https://static.zhongan.com/website/health/zarm/images/banners/1.png" alt="" />
+        <span className="menu-text">游戏专区</span>
+      </div>
+    </div>
   </div>
 };
 
