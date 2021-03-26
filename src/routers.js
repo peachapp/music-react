3f1fffd2624ca5c60650d174b045a16f6d8a9e3d
taotao <571596435@qq.com>
2021-03-26 10:40:57 +0800

    fix: 修复路由切换问题

--- src/routers.js
+++ src/routers.js
@@ -1,25 +1,49 @@
 import React, { Suspense, lazy } from 'react';
-import { HashRouter, Switch, Route, Redirect } from 'react-router-dom';
+import { HashRouter, Switch, Route, Redirect, withRouter } from 'react-router-dom';
 //HashRouter最外层必须由它包裹
 //Link用于点击跳转
 //Switch路由唯一匹配
 //Route配置路由规则
 //Redirect跳转错误显示的页面
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 import Loading from 'components/loading/index';
-import AnimationRoute from 'components/animationRoute/index';
-const Login = lazy(() => import('pages/login/index'));
-const Home = lazy(() => import('pages/home/index'));
-const Songrcmd = lazy(() => import('pages/songrcmd/index'));
-const Gedan = lazy(() => import('pages/gedan/index'));
-const Player = lazy(() => import('pages/player/index'));
-const Test = lazy(() => import('pages/test/index'));
-const NotFound = lazy(() => import('pages/notFound/index'));
+// const Login = lazy(() => import('pages/login/index'));
+// const Home = lazy(() => import('pages/home/index'));
+// const Songrcmd = lazy(() => import('pages/songrcmd/index'));
+// const Gedan = lazy(() => import('pages/gedan/index'));
+// const Player = lazy(() => import('pages/player/index'));
+// const Test = lazy(() => import('pages/test/index'));
+// const NotFound = lazy(() => import('pages/notFound/index'));
 
-const Routers = () => {
-  return <HashRouter>
-    <Suspense fallback={<Loading />}>
-      <AnimationRoute>
-        <Switch>
+import Login from 'pages/login/index';
+import Home from 'pages/home/index';
+import Songrcmd from 'pages/songrcmd/index';
+import Gedan from 'pages/gedan/index';
+import Player from 'pages/player/index';
+import Test from 'pages/test/index';
+import NotFound from 'pages/notFound/index';
+
+const AnimationRoute = withRouter(({ location }) => {
+  return (
+    <TransitionGroup
+      className={'router-container'}
+      childFactory={child => React.cloneElement(
+        child,
+        {
+          classNames: {
+            enter: 'animated',
+            enterActive: 'flipInY route-enter',
+            exit: 'animated',
+            exitActive: 'flipOutY route-exit',
+          }
+        }
+      )}
+    >
+      <CSSTransition
+        timeout={500}
+        key={location.pathname}
+      >
+        <Switch location={location}>
           <Route path='/' exact component={Home}></Route>
           <Route path='/home' component={Home}></Route>
           <Route path='/songrcmd' component={Songrcmd}></Route>
@@ -30,7 +54,15 @@ const Routers = () => {
           <Route path='/notFound' component={NotFound}></Route>
           <Redirect to='/notFound'></Redirect>
         </Switch>
-      </AnimationRoute>
+      </CSSTransition>
+    </TransitionGroup>
+  );
+});
+
+const Routers = () => {
+  return <HashRouter>
+    <Suspense fallback={<Loading />}>
+      <AnimationRoute />
     </Suspense>
   </HashRouter>
 };
