4ca8d2544579ac7f18a201b24ef9b4913b769d6d
taotao <571596435@qq.com>
2021-03-29 15:57:58 +0800

    fix: 修复使用a标签下载图片时打开图片而不是下载到本地的问题
    若想点击a标签就下载当前链接的图片，首先必须文件是同源的，在a标签上增加download属性，才能触发点击下载的效果，若不同源的话则变成在当前页面打开该图片了。若想要下载不同源的文件，有一种思路是将图片转为base6644再赋值给a标签，这样点击后应该就能下载了

--- src/components/gedanIntroduction/index.js
+++ src/components/gedanIntroduction/index.js
@@ -1,17 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Icon } from 'zarm';
 import "./index.less";
+
+const convertImgToBase64 = (url, callback, outputFormat) => {
+  var canvas = document.createElement('CANVAS'),
+    ctx = canvas.getContext('2d'),
+    img = new Image();
+  img.crossOrigin = 'Anonymous';
+  img.onload = function () {
+    canvas.height = img.height;
+    canvas.width = img.width;
+    ctx.drawImage(img, 0, 0);
+    var dataURL = canvas.toDataURL(outputFormat || 'image/png');
+    callback.call(this, dataURL);
+    canvas = null;
+  };
+  img.src = url;
+}
 
 const GedanIntroduction = (props) => {
   // props
   const { coverImgUrl, name, tags, description, onHide } = props || {};
 
+  // data
+  const [imgUrl, setImgUrl] = useState(coverImgUrl);
+
+  // 
+  useEffect(() => {
+    convertImgToBase64(coverImgUrl, function (base64Img) {
+      //转化后的base64
+      setImgUrl(base64Img);
+    });
+  }, [coverImgUrl]);
+
   // methods
   const onSaveCover = (event) => {
     event.stopPropagation();
     event.cancelBubble = true;
-
-    console.log('click');
   };
 
   return <div className="gedan-introduction-container" onClick={onHide}>
@@ -39,7 +64,7 @@ const GedanIntroduction = (props) => {
         }
       </div>
     </div>
-    <a className="gedan-introduction-save" href={coverImgUrl} download="封面图片" onClick={onSaveCover}>保存封面</a>
+    <a className="gedan-introduction-save" href={imgUrl} download="封面图片" onClick={onSaveCover}>保存封面</a>
     <div className="gedan-introduction-bg" style={{ 'backgroundImage': `url(${coverImgUrl})` }}></div>
     <div className="gedan-introduction-mask"></div>
   </div>
